b594d1ded03e5808b54868f412937043e52ca998
Egor And <georgand@yandex.ru>
2023-05-14 22:05:57 +0300

    Сделать сайт

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+let openPopup = document.querySelector('.profile__edit-button');
+let closePopup = document.querySelector('.popup__close-button');
+let editPopup = document.querySelector('.popup');
+
+let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('#name-input'); // Воспользуйтесь инструментом .querySelector()
+let aboutInput = formElement.querySelector('#about-input'); // Воспользуйтесь инструментом .querySelector()
+
+let pageTitle = document.querySelector('.profile__name');
+let pageAbout = document.querySelector('.profile__about-self');
+
+// Находим форму в DOM
+
+nameInput.value = pageTitle.textContent;
+aboutInput.value = pageAbout.textContent;
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  pageTitle.textContent = nameInput.value;
+  pageAbout.textContent = aboutInput.value;
+  closePopupFunction(editPopup);
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+openPopup.addEventListener('click', function () {
+  openPopupFunction(editPopup);
+});
+
+closePopup.addEventListener('click', function () {
+  closePopupFunction(editPopup);
+});
+
+function openPopupFunction(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopupFunction(popup) {
+  popup.classList.remove('popup_opened');
+}
