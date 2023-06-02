50c791e86a5e6db2548ef26650eb7edaac480a0e
AgeorgA <georgadn@yandex.ru>
2023-06-02 13:15:58 +0300

    Сделать вынесение карточек в отдельный файл

--- script/index.js
+++ script/index.js
@@ -1,35 +1,32 @@
-const openPopupFio = document.querySelector('.profile__edit-button');
-const openPopupMesto = document.querySelector('.profile__add-button');
+const buttonOpenPopupProfile = document.querySelector('.profile__edit-button');
+const buttonOpenPopupMesto = document.querySelector('.profile__add-button');
 
-// const closePopup = document.querySelector('.popup__close-button');
-const closePopupFio = document.querySelector('#close-fio');
-const closePopupMesto = document.querySelector('#close-card');
+const buttonClosePopupProfile = document.querySelector('#close-fio');
+const buttonClosePopupMesto = document.querySelector('#close-card');
+const buttonClosePopupZooming = document.querySelector('#close-big-image');
 
-// const editPopup = document.querySelector('.popup');
-const popupEditFio = document.querySelector('#popup_edit-fio');
-const popupAdd = document.querySelector('#popup_add-card');
+const popupProfile = document.querySelector('#popup_edit-fio');
+const popupMesto = document.querySelector('#popup_add-card');
+const popupImage = document.querySelector('#popup-image');
 
-const buttonName = document.querySelector('.popup__button');
-const popupName = document.querySelector('.popup__title');
-
-const formFio = document.querySelector('#popup__form_fio');
-const nameInput = formFio.querySelector('#name-input');
-const aboutInput = formFio.querySelector('#about-input');
+const formProfile = document.querySelector('#popup__form_fio');
+const nameInput = formProfile.querySelector('#name-input');
+const aboutInput = formProfile.querySelector('#about-input');
 
 const formCard = document.querySelector('#popup__form_card');
-const CardNameInput = formCard.querySelector('#card-name-input');
-const CardImgInput = formCard.querySelector('#card-img-input');
+const cardName = formCard.querySelector('#card-name-input');
+const cardImg = formCard.querySelector('#card-img-input');
 
-const pageTitle = document.querySelector('.profile__name');
-const pageAbout = document.querySelector('.profile__about-self');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about-self');
 
 const template = document.querySelector('#card-template');
 const templateContent = template.content;
 const card = templateContent.querySelector('.card');
 const cards = document.querySelector('.cards');
 
-const bigImagePlace = document.querySelector('#popup-image');
-const closeBigImageButton = document.querySelector('.popup-image__close');
+const zoomingImg = document.querySelector('.popup__image');
+const zoomingName = document.querySelector('.popup__name-image');
 
 const initialCards = [
   {
@@ -69,10 +66,10 @@ function createCard(nameCard, linkCard) {
   const newCard = card.cloneNode(true);
   const heart = newCard.querySelector('.card__heart');
   const deleteButton = newCard.querySelector('.card__trashbox');
-  const CardName = newCard.querySelector('.card__name');
+  const cardName = newCard.querySelector('.card__name');
   const imgEL = newCard.querySelector('.card__img');
 
-  CardName.textContent = nameCard;
+  cardName.textContent = nameCard;
   imgEL.src = linkCard;
   imgEL.alt = nameCard;
 
@@ -81,65 +78,50 @@ function createCard(nameCard, linkCard) {
   heart.addEventListener('click', () => heart.classList.toggle('card__heart_active'));
 
   imgEL.addEventListener('click', () => {
-    bigImagePlace.classList.add('popup_opened');
-    const bigImageImg = document.querySelector('.popup-image__image');
-    const bigImageName = document.querySelector('.popup-image__name');
+    openPopupFunction(popupImage);
 
-    bigImageImg.src = imgEL.src;
-    bigImageImg.alt = imgEL.alt;
-    bigImageName.textContent = CardName.textContent;
+    zoomingImg.src = imgEL.src;
+    zoomingImg.alt = imgEL.alt;
+
+    zoomingName.textContent = cardName.textContent;
   });
 
   return newCard;
 }
 
-function closeBigImagePlace(popup) {
-  popup.classList.remove('popup_opened');
-}
-
-function openPopupAdd(popup) {
-  popup.classList.add('popup_opened');
-  CardNameInput.value = null;
-  CardImgInput.value = null;
-}
-
 function openPopupFunction(popup) {
   popup.classList.add('popup_opened');
-  nameInput.value = pageTitle.textContent;
-  aboutInput.value = pageAbout.textContent;
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
 }
 
 function closePopupFunction(popup) {
   popup.classList.remove('popup_opened');
 }
 
-function closePopupAdd(popup) {
-  popup.classList.remove('popup_opened');
-}
-
-openPopupMesto.addEventListener('click', () => openPopupAdd(popupAdd));
-openPopupFio.addEventListener('click', () => openPopupFunction(popupEditFio));
-closePopupMesto.addEventListener('click', () => closePopupAdd(popupAdd));
-closePopupFio.addEventListener('click', () => closePopupFunction(popupEditFio));
-closeBigImageButton.addEventListener('click', () => closeBigImagePlace(bigImagePlace));
+buttonOpenPopupMesto.addEventListener('click', () => openPopupFunction(popupMesto));
+buttonOpenPopupProfile.addEventListener('click', () => openPopupFunction(popupProfile));
+buttonClosePopupMesto.addEventListener('click', () => closePopupFunction(popupMesto));
+buttonClosePopupProfile.addEventListener('click', () => closePopupFunction(popupProfile));
+buttonClosePopupZooming.addEventListener('click', () => closePopupFunction(popupImage));
 
 formCard.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  const valueName = CardNameInput.value;
-  const valueImg = CardImgInput.value;
+  const valueName = cardName.value;
+  const valueImg = cardImg.value;
   const newCard = createCard(valueName, valueImg);
 
   cards.prepend(newCard);
 
-  closePopupAdd(popupAdd);
+  closePopupFunction(popupMesto);
 });
 
-formFio.addEventListener('submit', function (evt) {
+formProfile.addEventListener('submit', function (evt) {
   evt.preventDefault();
 
-  pageTitle.textContent = nameInput.value;
-  pageAbout.textContent = aboutInput.value;
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutInput.value;
 
-  closePopupFunction(popupEditFio);
+  closePopupFunction(popupProfile);
 });
