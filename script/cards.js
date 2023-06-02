e0cd8a347205b96fd1d0d8e6d1a057d86aedb6b0
AgeorgA <georgadn@yandex.ru>
2023-06-02 13:17:19 +0300

    Сделать вынесение карточек в отдельный файл

---
+++ script/cards.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    nameCard: 'Архыз',
+    linkCard: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    nameCard: 'Челябинская область',
+    linkCard:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    nameCard: 'Иваново',
+    linkCard: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    nameCard: 'Камчатка',
+    linkCard: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    nameCard: 'Холмогорский район',
+    linkCard:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    nameCard: 'Байкал',
+    linkCard: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
--- script/index.js
+++ script/index.js
@@ -28,35 +28,6 @@ const cards = document.querySelector('.cards');
 const zoomingImg = document.querySelector('.popup__image');
 const zoomingName = document.querySelector('.popup__name-image');
 
-const initialCards = [
-  {
-    nameCard: 'Архыз',
-    linkCard: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    nameCard: 'Челябинская область',
-    linkCard:
-      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    nameCard: 'Иваново',
-    linkCard: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    nameCard: 'Камчатка',
-    linkCard: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    nameCard: 'Холмогорский район',
-    linkCard:
-      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    nameCard: 'Байкал',
-    linkCard: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 initialCards.forEach(function (item) {
   const newCard = createCard(item.nameCard, item.linkCard);
   cards.prepend(newCard);
